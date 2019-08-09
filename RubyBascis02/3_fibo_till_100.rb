55e15c608ba0181692572c6e65ae2d98bcbb8e33
Arakcheyev Alex <arakcheyev.alex@gmail.com>
2019-08-09 12:47:28 +0500

    Indents fixed

--- RubyBascis02/3_fibo_till_100.rb
+++ RubyBascis02/3_fibo_till_100.rb
@@ -1,7 +1,7 @@
 fibonacci_numbers = [0,1]
 
 while (next_number = fibonacci_numbers[-1] + fibonacci_numbers[-2]) < 100 do
-	fibonacci_numbers << next_number
+  fibonacci_numbers << next_number
 end
 
 puts fibonacci_numbers
