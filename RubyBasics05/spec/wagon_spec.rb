6d941f9496b432958cd2423410df25d51d068d69
Alex Arakcheyev <arakcheyev.alex@gmail.com>
2019-08-14 22:32:44 +0500

    Task 05. Different classes for wagons, user interface class added

--- RubyBasics05/spec/wagon_spec.rb
+++ RubyBasics05/spec/wagon_spec.rb
@@ -1,9 +1,10 @@
 require './wagon'
+require './cargo_wagon'
+require './passenger_wagon'
 
 describe Wagon do
   it 'create wagon with correct type' do
-    expect { Wagon.new(:some_type) }.to raise_error(ArgumentError, "Type must be only :cargo or :passenger")
-    expect(Wagon.new(:cargo).number).not_to be_empty
-    expect(Wagon.new(:passenger).number).not_to be_empty
+    expect(CargoWagon.new().type).to eq(:cargo)
+    expect(PassengerWagon.new().type).to eq(:passenger)
   end
 end
