33158ad2adf630714bd4ea25c5f06b38dc1d8eac
Alex <arakcheyev.alex@gmail.com>
2019-08-28 19:06:39 +0700

    Task 10 fixes

--- RubyBasics10/accessors.rb
+++ RubyBasics10/accessors.rb
@@ -2,22 +2,18 @@ module Accessors
   def attr_accessor_with_history(*names)
     names.each do |name|
       attr_reader name
+      attr_reader "#{name}_history".to_sym
 
       attr_name = "@#{name}".to_sym
       attr_history_name = "@#{name}_history".to_sym
 
-      # history reader
-      define_method("#{name}_history") do
-        instance_variable_get(attr_history_name)
-      end
-
       # history attr_writer
       define_method("#{name}=") do |value|
-        instance_variable_set(attr_name, value)
         history = instance_variable_get(attr_history_name)
         history ||= []
-        history << value
+        history << instance_variable_get(attr_name)
         instance_variable_set(attr_history_name, history)
+        instance_variable_set(attr_name, value)
       end
     end
   end
